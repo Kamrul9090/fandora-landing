03ca50482e80973684bdfca06be43f026df309b1
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2023-05-14 23:36:52 +0600

    added app.css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,24 @@
+import Card from "../Card/Card";
+import Content from "../Content/Content";
+import ContentCard from "../ContentCard/ContentCard";
+import Discover from "../Discover/Discover";
+import Product from "../Product/Product";
+import ServiceCheck from "../ServiceCheck/ServiceCheck";
+import Banner from "./Banner/Banner";
+import SearchBar from "./SearchBar/SearchBar";
+
+
 const Home = () => {
     return (
         <div>
-            <h1>this is home</h1>
+            <Banner></Banner>
+            <SearchBar></SearchBar>
+            <ContentCard></ContentCard>
+            <Card></Card>
+            <Product></Product>
+            <ServiceCheck></ServiceCheck>
+            <Content></Content>
+            <Discover></Discover>
         </div>
     );
 };
