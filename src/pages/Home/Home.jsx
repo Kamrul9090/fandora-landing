3f1d92b3eb2d9a0e7975c7c1bd59739610970519
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2023-05-15 15:22:06 +0600

    added server in confog file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,24 @@
-// import Card from "../Card/Card";
-// import Content from "../Content/Content";
-// import ContentCard from "../ContentCard/ContentCard";
-// import Discover from "../Discover/Discover";
-// import Product from "../Product/Product";
-// import ServiceCheck from "../ServiceCheck/ServiceCheck";
-// import Banner from "./Banner/Banner";
-// import SearchBar from "./SearchBar/SearchBar";
+import Card from "../Card/Card";
+import Content from "../Content/Content";
+import ContentCard from "../ContentCard/ContentCard";
+import Discover from "../Discover/Discover";
+import Product from "../Product/Product";
+import ServiceCheck from "../ServiceCheck/ServiceCheck";
+import Banner from "./Banner/Banner";
+import SearchBar from "./SearchBar/SearchBar";
 
 
 const Home = () => {
     return (
         <div>
-            <h1>thos s home</h1>
-            {/* <Banner></Banner>
+            <Banner></Banner>
             <SearchBar></SearchBar>
             <ContentCard></ContentCard>
             <Card></Card>
             <Product></Product>
             <ServiceCheck></ServiceCheck>
             <Content></Content>
-            <Discover></Discover> */}
+            <Discover></Discover>
         </div>
     );
 };
